6d6dd6086cb64b191963944f63885b4c87771d0a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-09-16 09:49:32 -0400

    Added some comments explaining the platform module.

--- nativescript/app/platform/platform.module.ts
+++ nativescript/app/platform/platform.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 
-
+/**
+ * This module contains platform specific imports. This version holds any mobile-specific imports. A different file
+ * by the same name appears in the src directory that will load imports specific to the web platform.
+ */
 @NgModule({
   imports: [],
   exports: []
--- src/client/platform/platform.module.ts
+++ src/client/platform/platform.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import {MaterialModule} from "./material/material.module";
 
 
+/**
+ * This module contains platform specific imports. This version holds any web-only imports. A different file
+ * by the same name appears in the nativescript directory that will not load material design imports.
+ */
 @NgModule({
   imports: [MaterialModule.forRoot()],
   exports: [MaterialModule]
